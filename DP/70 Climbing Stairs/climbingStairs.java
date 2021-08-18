3fa0571a842143e345efc085862f8c1d12fa4b45
nataliesty <natalie_soo@hotmail.com>
2021-08-18 12:47:52 -0700

    70 dp

---
+++ DP/70 Climbing Stairs/climbingStairs.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int climbStairs(int n) {
+        int[] stairs = new int[n];
+        
+        stairs[0] = 1;
+        if(n==1){return 1;}
+        stairs[1] = 2;
+        
+        for(int i = 2; i<n; i++){
+            stairs[i] = stairs[i-1] +stairs[i-2];
+        }
+        
+        return stairs[n-1];
+    }
+}
