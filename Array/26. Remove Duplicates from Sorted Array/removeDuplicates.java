56a841c960d1672ae8451e3696af85b1b592b8fc
nataliesty <natalie_soo@hotmail.com>
2021-08-22 20:38:57 -0700

    26

---
+++ Array/26. Remove Duplicates from Sorted Array/removeDuplicates.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int p1=0;
+        int p2=0;
+        
+        while(p2<nums.length){
+            if(nums[p1] != nums[p2]){
+                p1++;
+                nums[p1] = nums[p2];
+                
+            }
+            p2++;
+        }
+        
+        return p1+1;
+    }
+}
